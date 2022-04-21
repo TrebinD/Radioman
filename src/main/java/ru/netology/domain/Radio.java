c628d5a01747d785383a27a5c1de1f2c07327752
Dmitrii Trebin <d.trebin@rambler.ru>
2022-04-21 19:09:51 +0300

    Add new constructor

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -4,27 +4,37 @@ public class Radio {
 
     private int numStation;
     private int volume;
+    private int maxNumberStation = 9;
+    private final int minNumberStation = 0;
+    private final int minVolume = 0;
+    private final int maxVolume = 100;
 
+
+    public Radio() {
+    }
+
+    public Radio(int maxNumberStation) {
+        this.maxNumberStation = maxNumberStation - 1;
+    }
 
     public int getNumStation() {
         return numStation;
     }
 
     public void setNumStation(int numStation) {
-        if (numStation < 0) {
-            numStation = 0;
+        if (numStation > maxNumberStation) {
+            this.numStation = maxNumberStation;
             return;
         }
-        if (numStation > 9) {
-            numStation = 0;
+        if (numStation < minNumberStation) {
             return;
         }
         this.numStation = numStation;
     }
 
     public void setNextStation() {
-        if (numStation == 9) {
-            numStation = 0;
+        if (numStation >= maxNumberStation) {
+            numStation = minNumberStation;
             return;
         }
         numStation++;
@@ -32,8 +42,8 @@ public class Radio {
     }
 
     public void setPrevStation() {
-        if (numStation == 0) {
-            numStation = 9;
+        if (numStation <= minNumberStation) {
+            numStation = maxNumberStation;
             return;
         }
         numStation--;
@@ -45,32 +55,31 @@ public class Radio {
         return volume;
     }
 
-    public void setVolumeStation(int volume) {
-        if (volume > 10) {
-            this.volume = 10;
+    public void setVolume(int volume) {
+        if (volume > maxVolume) {
+            this.volume = maxVolume;
             return;
         }
-        if (volume < 0) {
-            this.volume = 0;
+        if (volume < minVolume) {
+            this.volume = minVolume;
             return;
         }
         this.volume = volume;
-
     }
 
+
     public void setMinLevelVolume() {
-        if (this.volume == 0) {
+        if (volume == minVolume) {
             return;
         }
         this.volume--;
     }
 
     public void setMaxLevelVolume() {
-        if (this.volume == 10) {
-            this.volume = 10;
+        if (volume == maxVolume) {
             return;
         }
-        this.volume++;
+        volume++;
     }
 
 
--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -7,13 +7,13 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
-    Radio radio = new Radio();
+    Radio radio = new Radio(55);
 
     @ParameterizedTest
     @CsvSource(value = {" 'Number Station',4,4",
             " 'Min Number Station',0,0",
-            " 'Max Number Station',9,9",
-            " 'Up Limit Number Station', 10,0",
+            " 'Max Number Station',54,54",
+            " 'Up Limit Number Station', 55,54",
             " 'Down Limit Number Station', -1,0"})
     public void numberStation(String name, int incomingData, int expected) {
         radio.setNumStation(incomingData);
@@ -23,8 +23,8 @@ class RadioTest {
 
 
     @ParameterizedTest
-    @CsvSource(value = {" 'Up Limit Next Station',9,0",
-            " 'NextStation',0,1 "})
+    @CsvSource(value = {" 'Up Limit Next Station',54,0",
+            " 'Next Station',24,25 "})
     public void nextStation(String name, int incomingData, int expected) {
         radio.setNumStation(incomingData);
         radio.setNextStation();
@@ -33,8 +33,8 @@ class RadioTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {" 'PrevStation',9,8",
-            " 'Down Limit Next Station',0,9 "})
+    @CsvSource(value = {" 'Prev Station',9,8",
+            " 'Down Limit Next Station',0,54 "})
     public void prevStation(String name, int incomingData, int expected) {
         radio.setNumStation(incomingData);
         radio.setPrevStation();
@@ -45,10 +45,10 @@ class RadioTest {
     @ParameterizedTest
     @CsvSource(value = {" 'Level Volume',5,5",
             " 'Min Border Volume',-1,0",
-            " 'Max Border Volume',11,10",
+            " 'Max Border Volume',101,100",
     })
     public void levelVolume(String name, int incomingData, int expected) {
-        radio.setVolumeStation(incomingData);
+        radio.setVolume(incomingData);
         int actual = radio.getVolume();
         assertEquals(expected, actual);
     }
@@ -58,21 +58,91 @@ class RadioTest {
     @CsvSource(value = {" 'Min Level Volume',0,0",
             " 'Down Level Volume',6,5"
     })
-    public void minLevelVolume(String name, int incomingData, int expected) {
-        radio.setVolumeStation(incomingData);
+    public void downLevelVolume(String name, int incomingData, int expected) {
+        radio.setVolume(incomingData);
         radio.setMinLevelVolume();
         int actual = radio.getVolume();
         assertEquals(expected, actual);
     }
 
     @ParameterizedTest
-    @CsvSource(value = {" 'Max Level Volume',11,10",
-            " 'Up Level Volume',5,6"
+    @CsvSource(value = {" 'Max Level Volume',100,100",
+            " 'Up Level Volume',21,22",
     })
-    public void maxLevelVolume(String name, int incomingData, int expected) {
-        radio.setVolumeStation(incomingData);
+    public void upLevelVolume(String name, int incomingData, int expected) {
+        radio.setVolume(incomingData);
         radio.setMaxLevelVolume();
         int actual = radio.getVolume();
         assertEquals(expected, actual);
     }
+
+    Radio defaultOnly = new Radio();
+
+    @ParameterizedTest
+    @CsvSource(value = {" 'Number Station Default',4,4",
+            " 'Min Number Station Default',0,0",
+            " 'Max Number Station Default',9,9",
+            " 'Up Limit Number Station Default', 10,9",
+            " 'Down Limit Number Station Default', -1,0"})
+    public void WithoutSettingNumberStation(String name, int incomingData, int expected) {
+        defaultOnly.setNumStation(incomingData);
+        int actual = defaultOnly.getNumStation();
+        assertEquals(expected, actual);
+    }
+
+
+    @ParameterizedTest
+    @CsvSource(value = {" 'Up Limit Next Station Default',10,0",
+            " 'Next Station Default',0,1 "})
+    public void WithoutSettingNextStation(String name, int incomingData, int expected) {
+        defaultOnly.setNumStation(incomingData);
+        defaultOnly.setNextStation();
+        int actual = defaultOnly.getNumStation();
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {" 'Prev Station Default',9,8",
+            " 'Down Limit Next Station Default',0,9 "})
+    public void WithoutSettingPrevStation(String name, int incomingData, int expected) {
+        defaultOnly.setNumStation(incomingData);
+        defaultOnly.setPrevStation();
+        int actual = defaultOnly.getNumStation();
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {" 'Level Volume Default',5,5",
+            " 'Min Border Volume Default',-1,0",
+            " 'Max Border Volume Default',101,100",
+    })
+    public void WithoutSettingLevelVolume(String name, int incomingData, int expected) {
+        defaultOnly.setVolume(incomingData);
+        int actual = defaultOnly.getVolume();
+        assertEquals(expected, actual);
+    }
+
+
+    @ParameterizedTest
+    @CsvSource(value = {" 'Min Level Volume Default',0,0",
+            " 'Down Level Volume Default',6,5"
+    })
+    public void WithoutSettingDownLevelVolume(String name, int incomingData, int expected) {
+        defaultOnly.setVolume(incomingData);
+        defaultOnly.setMinLevelVolume();
+        int actual = defaultOnly.getVolume();
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {" 'Max Level Volume Default',100,100",
+            " 'Up Level Volume Default',21,22",
+    })
+    public void WithoutSettingUpLevelVolume(String name, int incomingData, int expected) {
+        defaultOnly.setVolume(incomingData);
+        defaultOnly.setMaxLevelVolume();
+        int actual = defaultOnly.getVolume();
+        assertEquals(expected, actual);
+    }
+
 }
